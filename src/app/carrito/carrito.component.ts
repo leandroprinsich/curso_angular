f6b65c3bbe6fdcd6841937216d8ebd94fd16cfe0
leandroprinsich <leandroprinsich@gmail.com>
2021-09-30 11:31:52 -0300

    ejercicio terminado

---
+++ src/app/carrito/carrito.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Producto } from 'src/models/producto';
+
+@Component({
+  selector: 'app-carrito',
+  templateUrl: './carrito.component.html',
+  styleUrls: ['./carrito.component.css']
+})
+export class CarritoComponent implements OnInit {
+  @Input() productosAgregados: Array<Producto>;
+
+  @Input() totalCarrito: number = 0;
+
+  constructor() {
+    this.productosAgregados = [];
+  }
+
+  onDeleteProducto(item: Producto){
+    if(item.agregados <= 1){
+      this.productosAgregados = this.productosAgregados.filter(e => e.id != item.id);
+    }
+    item.agregados--;
+  }
+
+  calcularTotal():number {
+    let resultado = 0;
+    this.productosAgregados.map(e => resultado += parseInt(e.precio)*e.agregados);
+    return resultado;
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
+//

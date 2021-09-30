cf27431aa44db32c5a6b47ed93164f296fe9e9d7
leandroprinsich <leandroprinsich@gmail.com>
2021-09-30 15:47:01 -0300

    ejercicio carrito compras terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
-
+import { Producto } from 'src/models/producto';
+import fuente from './fuenteDatos/fuente.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicioRestaurante';
+  aBebidas: Array<Producto>;
+  aComidas: Array<Producto>;
+  productosAgregados: Array<Producto>;
+
+  constructor(){
+    this.productosAgregados = [];
+    this.aBebidas = fuente.filter(e => e.categoria == "BEBIDA");
+    this.aComidas = fuente.filter(e => e.categoria == "COMIDA");
+  }
+
+  onProductoAgregado(prod:any){
+    let repetido = this.productosAgregados.includes(prod)
+    if(!repetido){
+      this.productosAgregados.push(prod);
+    }
+    prod.agregados++;
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaProductosComponent } from './lista-productos/lista-productos.component';
+import { CarritoComponent } from './carrito/carrito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductosComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule
